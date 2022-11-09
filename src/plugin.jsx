db103e69ee0ede3215e9361d97dbb9f8ba447bd9
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-11-10 08:53:13 +1100

    simplify plugin

--- src/plugin.jsx
+++ src/plugin.jsx
@@ -1,11 +1,4 @@
 import { definePlugin } from '@mkeeorg/federation-ui';
-import Help from './help';
+// import Help from './help';
 
-export default definePlugin({
-  fillItems: [
-    {
-      slotId: 'careerPrelist',
-      component: Help,
-    },
-  ],
-});
+export default definePlugin({});
